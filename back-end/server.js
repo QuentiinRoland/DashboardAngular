7f055d97ccc7273d41295b89498c82dfeb36d239
QuentiinRoland <quentinroland12@gmail.com>
2025-03-11 22:38:39 +0100

    Fix: API listening on correct port

--- back-end/server.js
+++ back-end/server.js
@@ -12,7 +12,14 @@ app.use(express.json());
 
 app.use("/customers", customersRoutes);
 
-const PORT = process.env.PORT || 5001;
+app.get("/", (req, res) => {
+  res.json({ message: "API en ligne 🚀" });
+});
+
+
+const PORT = process.env.PORT || 8080;
+const API_URL = process.env.PUBLIC_API_URL;
+
 app.listen(PORT, () =>
   console.log(`Serveur lancé sur http://localhost:${PORT}`)
 );
